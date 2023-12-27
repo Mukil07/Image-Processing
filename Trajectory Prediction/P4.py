ff698b14b65973561519eed5988603cebe767092
Mukilan <98142757+Mukil07@users.noreply.github.com>
2023-12-27 10:35:03 +0530

    Update P4.py

--- Trajectory Prediction/P4.py
+++ Trajectory Prediction/P4.py
@@ -113,66 +113,7 @@ while(vid.isOpened()):
    else:
       break
 
-initial= cv2.imread('initial.jpg')
-pic1= cv2.imread('1.jpg')
-pic2= cv2.imread('2.jpg')
-pic3= cv2.imread('3.jpg')
-pic4= cv2.imread('6.jpg')
-pic5= cv2.imread('8.jpg')
-pic= cv2.imread('cool12.jpg')
-pic6= cv2.imread('10.jpg')
-pic7= cv2.imread('11.jpg')
-pic8= cv2.imread('cool13.jpg')
-
-  
-ret,thresh= cv2.threshold(pic1,3,255,cv2.THRESH_BINARY)
-thresh_inv1= cv2.bitwise_not(thresh)
-ret,thresh= cv2.threshold(pic2,3,255,cv2.THRESH_BINARY)
-thresh_inv2= cv2.bitwise_not(thresh)
-ret,thresh= cv2.threshold(pic3,3,255,cv2.THRESH_BINARY)
-thresh_inv3= cv2.bitwise_not(thresh)
-
-ret,thresh= cv2.threshold(pic4,5,255,cv2.THRESH_BINARY)
-thresh_inv6= cv2.bitwise_not(thresh)
-
-ret,thresh= cv2.threshold(pic5,5,255,cv2.THRESH_BINARY)
-thresh_inv8= cv2.bitwise_not(thresh)
-
-ret,thresh= cv2.threshold(pic,5,255,cv2.THRESH_BINARY)
-thresh_inv9= cv2.bitwise_not(thresh)
-
-ret,thresh= cv2.threshold(pic8,5,255,cv2.THRESH_BINARY)
-thresh_inv7= cv2.bitwise_not(thresh)
-
-ret,thresh= cv2.threshold(pic6,5,255,cv2.THRESH_BINARY)
-thresh_inv10= cv2.bitwise_not(thresh)
-
-ret,thresh= cv2.threshold(pic7,5,255,cv2.THRESH_BINARY)
-thresh_inv11= cv2.bitwise_not(thresh)
-
-
-img1= cv2.bitwise_and(initial,thresh_inv1)
-img2= cv2.bitwise_and(img1,thresh_inv2)
-img3= cv2.bitwise_and(img2,thresh_inv3)
-img4= cv2.bitwise_and(img3,thresh_inv6)
-img5= cv2.bitwise_and(img4,thresh_inv8)
-img6= cv2.bitwise_and(img5,thresh_inv10)
-img7= cv2.bitwise_and(img6,thresh_inv11)
-img8= cv2.bitwise_and(img7,thresh_inv9)
-img9= cv2.bitwise_and(img8,thresh_inv7)
-
-
-
-final=img9+pic1+pic2+pic3+pic4+pic5+pic6+pic7+pic+pic8
-#for i in range(1,12):
-   #final= initial+cv2.imread(r'i')
-
-
-cv2.imshow('final',final)
 cv2.waitKey(0)
-
-
-      
 vid.release()
 cv2.destroyAllWindows()
    
